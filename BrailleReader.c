15ac78a2e9d0af2036f855d9272040ed2a9be27b
Yasmin <yasmin@carmo.in>
2019-11-13 18:52:31 -0300

    Versão 1.1

--- BrailleReader.c
+++ BrailleReader.c
@@ -13,35 +13,12 @@ void inicializar()
     inicializa_letras();
     file_write();
     
-    //configurando entrada e saída pinos
+    //configurando entrada e saida pinos
     
-    //Pinos 1
-    pinMode(1, INPUT);
-    pullUpDnControl(1, PUD_UP);
-    
-    //Pinos 2
-    pinMode(2, INPUT);
-    pullUpDnControl(2, PUD_UP);
-    
-    //Pinos 3
-    pinMode(3, INPUT);
-    pullUpDnControl(3, PUD_UP);
-    
-    //Pinos 4
-    pinMode(4, INPUT);
-    pullUpDnControl(4, PUD_UP);
-    
-    //Pinos 5
-    pinMode(5, INPUT);
-    pullUpDnControl(5, PUD_UP);
-    
-    //Pinos 6
-    pinMode(6, INPUT);
-    pullUpDnControl(6, PUD_UP);
-    
-    //Botao principal
-    pinMode(7, INPUT);
-    pullUpDnControl(7, PUD_UP);
+    for (i = 1; i <= 7; i++) {
+        pinMode(i, INPUT);
+        pullUpDnControl(i, PUD_UP);
+    }
 }
 
 int checarbotao(int botao)
@@ -49,8 +26,7 @@ int checarbotao(int botao)
     if (digitalRead(botao) == LOW)
         return 1;
     else 
-        return 0;
-    
+        return 0; 
 }
 
 void checarpinos(int checkpinos[])
@@ -62,7 +38,7 @@ void checarpinos(int checkpinos[])
 
 void ExecutarAudio (char ret)
 {
-// strcat so aceita string
+    // strcat so aceita string
     char letra_ret[1]; 
     letra_ret[0] = ret; // letra retornada
     char mp3[7] = ".mp3 &"; // extensao
@@ -81,22 +57,20 @@ void ExecutarAudio (char ret)
     strcpy(letra_ret,"");
 }
     
-void executar()
+void ModoLivre()
 {
     int checkpinos[7] = {0}, j; //{0,1,0,0,0,0,0}
     char ret = ' ';
-    //ativa todos os pinos, somente enquanto o botão principal estiver sendo pressionado
+    //ativa todos os pinos, somente enquanto o botï¿½o principal estiver sendo pressionado
     while (TRUE) {
         if(digitalRead(7) == LOW) {
             checarpinos(checkpinos);
             
             ret = buscaLetra(checkpinos);
-            for(j = 1; j <= 6; j++) {
+            for(j = 1; j <= 6; j++)
                 printf("pino %d: %d\n", j, checkpinos[j]);
-            }
-            if(!ret) {
+            if(!ret)
                 printf("\n\nErro ao buscar letra na biblioteca!!\n\n");
-            }
             else {
                 printf("\nLetra encontrada: %c\n\n", ret);
                 ExecutarAudio(ret);
@@ -109,7 +83,7 @@ void executar()
 int main()
 {
     inicializar();
-    executar();
+    ModoLivre ();
     return 0;
 }
 
