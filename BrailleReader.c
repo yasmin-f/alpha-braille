0315464c92d382c6e3780674596e9906f5206b61
GiseleVitoria <36933423+GiseleVitoria@users.noreply.github.com>
2019-12-04 10:36:01 -0300

    Add files via upload

--- BrailleReader.c
+++ BrailleReader.c
@@ -10,18 +10,17 @@
 #define MAXMODES 2
 
 int const MainButton = 7;
-int const ModeSelect = 8;
+int const ModeSelect = 6;
 
 void inicializar ()
 {
     int i;
     wiringPiSetup();
-    
+
     //puxa banca de dados
     inicializa_letras();
-    file_write();
-    
-    //configurando pinos de entrada e saida 
+
+    //configurando pinos de entrada e saida
     for (i = 1; i <= 7; i++) {
         pinMode(i, INPUT);
         pullUpDnControl(i, PUD_UP);
@@ -31,7 +30,7 @@ void inicializar ()
 int checarbotao (int botao)
 {
     if (digitalRead (botao) == LOW) return 1;
-    else return 0; 
+    else return 0;
 }
 
 void checarpinos (int checkpinos[])
@@ -41,47 +40,61 @@ void checarpinos (int checkpinos[])
         checkpinos [i] = checarbotao(i);
 }
 
+/***
+        0 - voce errou a letra
+        1 - voce acertou a letra
+        3 - Coloque os pinos correspondentes a letra
+        4 - Erro ao buscar letra na biblioteca
+***/
+
 void ExecutarAudio (char ret)
 {
-    // strcat so aceita string
-    char letra_ret[1]; 
-    letra_ret[0] = ret; // letra retornada
-    char mp3[7] = ".mp3 &"; // extensao
-    char str[50] = "omxplayer -o hdmi /home/pi/Projeto/Audios/"; // comando
+    /// strcat so aceita string
+    char letra_ret[1];
 
-    // concatena as strings - strcat()
+    /// letra retornada
+    letra_ret[0] = ret;
+    letra_ret[1] = '\0';
+    /// extensao
+    char mp3[7] = ".mp3 &";
+    /// comando
+    char str[50] = "omxplayer -o hdmi /home/pi/Projeto/Audios/";
+
+    /// concatena as strings - strcat()
     strcat(str,letra_ret);
     strcat(str, mp3);
 
-    //Executa 
+    ///Executa
     system(str);
 
-    // apaga tudo
+    /// apaga tudo
     strcpy(str,"");
     strcpy(mp3,"");
     strcpy(letra_ret,"");
 }
-    
+
 void ModoLivre ()
 {
-    int i, checkpinos[MAXP] = {0}; //{0,1,0,0,0,0,0}
+    int i, checkpinos[MAXP] = {0};
     char ret = ' ';
 
-    //ativa todos os pinos, somente enquanto o botao principal estiver sendo pressionado
+    ///ativa todos os pinos, somente enquanto o botao principal estiver sendo pressionado
     while (TRUE) {
         if(digitalRead (MainButton) == LOW) {
             checarpinos (checkpinos);
-            
             ret = buscaLetra (checkpinos);
-            for (i = 1; i <= 6; i++)
+            /*for (i = 1; i <= 6; i++)
                 printf ("pino %d: %d\n", i, checkpinos[i]);
+            */
             if (!ret)
-                printf ("\n\nErro ao buscar letra na biblioteca!!\n\n");
+                ///printf ("\n\nErro ao buscar letra na biblioteca!!\n\n");
+                ExecutarAudio ('4');
+                // vc errou a letra???
             else {
-                printf ("\nLetra encontrada: %c\n\n", ret);
+                ///printf ("\nLetra encontrada: %c\n\n", ret);
                 ExecutarAudio (ret);
             }
-            delay(300);         
+            delay(300);
         }
     }
 }
@@ -92,13 +105,14 @@ void ModoAtividade()
     int i, j, checkpinos[MAXP] = {0}; //{0,1,0,0,0,0,0}
     char randletra, ret = ' ';
 
-    //Randomiza letra
+    ///Randomiza letra
     randletra = rand() % 26 + 97;
-    printf("\nColoque os pinos correspondentes a letra: %c\n\n", randletra);
+   // printf("\nColoque os pinos correspondentes a letra: %c\n\n", randletra);
+    ExecutarAudio('3');
     ExecutarAudio(randletra);
     delay(300);
 
-    //ativa todos os pinos, somente enquanto o botao principal estiver sendo pressionado
+    ///ativa todos os pinos, somente enquanto o botao principal estiver sendo pressionado
     while (TRUE)
     {
         if (digitalRead(MainButton) == LOW)
@@ -106,22 +120,25 @@ void ModoAtividade()
             for (i = 0; i < 3; i++) {
                 checarpinos(checkpinos);
                 ret = buscaLetra(checkpinos);
-                for (j = 0; j < 6; j++)
+               /* for (j = 0; j < 6; j++)
                     printf("pino %d: %d\n", j, checkpinos[j]);
+                */
                 if (randletra == ret)
                 {
-                    printf("\nLetra encontrada: %c\n", ret);
-                    printf("Voce acertou\n\n");
-                    ExecutarAudio(ret);
+                    //printf("\nLetra encontrada: %c\n", ret);
+                    //printf("Voce acertou\n\n");
+                    ExecutarAudio('1');
                     delay(1000);
                     break;
                 }
                 else
-                    printf("\nLetra encontrada: %c\n", ret);
-                    printf("Voce errou a letra!!\n\n");
+                    //printf("\nLetra encontrada: %c\n", ret);
+                    //printf("Voce errou a letra!!\n\n");
+                    ExecutarAudio('0');
             }
             randletra = rand() % 26 + 97;
-            printf("\nColoque os pinos correspondentes a letra: %c\n\n", randletra);
+            //printf("\nColoque os pinos correspondentes a letra: %c\n\n", randletra);
+            ExecutarAudio('3');
             ExecutarAudio(randletra);
             delay(300);
         }
@@ -138,7 +155,7 @@ int main()
     inicializar ();
     while (true) {
         if (digitalRead (ModeSelect) == LOW) ((modenum++) % MAXMODES);
-        
+
         if (modenum == 0) ModoLivre (checkpinos);
         if (modenum == 1) ModoAtividade (checkpinos);
     }
--- banco_letras.h
+++ banco_letras.h
@@ -4,18 +4,11 @@
 
 //#include "C:\C_Progressivo\mystring.h"
 
-// Estrutura de dados
-//struct
-
+/// Estrutura de dados
 typedef struct {
    int pino[7];
    char letra;
-} LETRAS
-
-typedef struct {
-   int pino[7];
-   char letra;
-} l[40];
+} LETRAS;
 
 void inicializa_letras()
 {
@@ -45,133 +38,30 @@ void inicializa_letras()
                        {0,1,0,1,1,1},       //w
                        {1,0,1,1,0,1},       //x
                        {1,0,1,1,1,1},       //y
-                       {1,0,1,0,1,0};       //z
-                       {1,1,1,1,0,1},      
-                       {1,1,1,0,1,1},
-                       {1,0,0,0,0,1},
-                       {1,1,0,1,0,1},
-                       {0,0,1,1,1,0},
-                       {1,1,1,1,1,1},
-                       {1,1,0,0,0,1},
-                       {0,0,1,1,0,0},
-                       {1,1,0,1,1,1},
-                       {0,1,0,1,0,1},
-                       {0,0,1,1,0,1},
-                       {1,0,0,1,1,1},
-                       {0,1,1,1,1,1},
-                       {1,1,0,0,1,1}};
+                       {1,0,1,0,1,0}};      //z
+
 
     LETRAS alfabeto [40];
     for (i = 0; i < 26; i++) {
         for (j = 0; j < 6; j++) {
             alfabeto[i].pino[j] = aux[i][j];
             alfabeto[i].letra = 'a' + i;
-            printf("%d", letra[i].pino[j]);
+            //printf("%d", letra[i].pino[j]);
         }
-        printf(" - %d\n", i);
+        //printf(" - %d\n", i);
     }
-
-    //cedilha {1,1,1,1,0,1}
-    alfabeto[26].letra='ç';
-
-    //A com agudo {1,1,1,0,1,1}
-    alfabeto[27].letra='á';
-
-    //A com circunflexo {1,0,0,0,0,1}
-    alfabeto[28].letra='â';
-
-    //A com crase {1,1,0,1,0,1}
-    alfabeto[29].letra='à';
-
-    //( {0,0,1,1,1,0}
-    alfabeto[30].letra=')';
-
-    //e com agudo {1,1,1,1,1,1}
-    alfabeto[31].letra='é';
-
-    //e com circunflexo {1,1,0,0,0,1}
-    alfabeto[32].letra='ê';
-
-    //i com agudo {0,0,1,1,0,0}
-    alfabeto[33].letra='í';
-
-    //� {1,1,0,1,1,1}
-    alfabeto[34].letra='�';
-
-    //� {0,1,0,1,0,1}
-    alfabeto[35].letra='�';
-
-    //� {0,0,1,1,0,1}
-    alfabeto[36].letra='�';
-
-    //� {1,0,0,1,1,1}
-    alfabeto[37].letra='�';
-
-    //� {0,1,1,1,1,1}
-    alfabeto[38].letra='�';
-
-    //� {1,1,0,0,1,1}
-    alfabeto[39].letra='�';
-
-}
-void file_write() {
-    // Arquivo
-    // gravando no arquivo
-    int gravado; 
-    FILE *arquivo;
-
-    arquivo = fopen ("alfabeto.txt", "wb");//wb - arquivo binario
-    if (arquivo == NULL) {
-        printf ("\nArquivo alfabeto.txt n�o pode ser aberto.\n");
-        system ("PAUSE");
-    }
-    else {
-        gravado = fwrite (&letra, sizeof(struct letra), 40, arquivo);
-        if (gravado != 40){
-            printf ("\nErro ao gravar no arquivo.\n");
-            system ("PAUSE");
-        }
-    }
-    fclose(arquivo);
-}
-
-void file_read(){
-    // lendo o arquivo
-    int i, j;
-
-    FILE *arquivo;
-    arquivo = fopen("alfabeto.txt", "rb");
-    if (arquivo == NULL){
-        printf("\nErro na abertura do arquivo\n");
-        system("PAUSE");
-    }
-    else {
-
-        //verificar se esta pegando do arquivo binario
-        // l e outra struct que vai receber minha estrutura gravada no arquivo e implimila
-        // antes l era &letra
-        fread (&l, sizeof(struct LETRA), 40, arquivo);
-        for (i = 0; i <= 40; i++) {
-                printf("\n\n\nLetra: %c\n", l[x].letra);
-                printf("\nPinos: \n");
-                printf("\nP1 | P2 | P3 | P4 | P5 | P6 \n");
-            for(j = 0; j < 6; j++)
-                printf("%d    ",l[x].pino[j]);
-            }
-        }
-    fclose(arquivo);
 }
 
 char buscaLetra(int checkpinos[]){
     int res, i, j;
     char retorno = NULL;
-
+    LETRAS letra[40];
     // i - contador para letras
     for(i = 0; i < 40; i++){
         // j - contador para os pinos das letras
         for (res = j = 0; j < 6; j++) {
             if(checkpinos[j] == letra[i].pino[j]) res=1;
-            else break; 
+            else break;
             // se ele nao for igual ja saio da repeticao de pinos (j)
 
             // se o j estiver na ultima posicao e o res==1 retorno a letra
@@ -183,33 +73,4 @@ char buscaLetra(int checkpinos[]){
     return retorno = '\0'; // se ele passar todas as letras e nao encontrar nenhuma retorno nao encontrado
 }
 
-char busca_letra_file_bin(int checkpinos[]){
-    // lendo o arquivo
-    FILE *arquivo;
-    arquivo = fopen("alfabeto.txt","rb");
-    if (arquivo == NULL){
-        printf("\nErro na abertura do arquivo\n");
-        system("PAUSE");
-    }
-    else{
-        //verificar se esta pegando do arquivo binario
-        // l e outra struct que vai receber minha estrutura gravada no arquivo e implimila
-        // antes l era &letra
-        fread(&l, sizeof(struct letra), 40, arquivo);
-        int i=0,j=0,res=0; char ret = NULL;
-        for(i=1;i<=40;i++){
-            res=0;
-            j=1;
-            while(j<=6){
-                if(checkpinos[j] == l[i].pino[j])
-                    res=1;
-                else break;
-                if(j==6 && res)
-                    return ret = l[i].letra;
-                j++;
-            }
-        }
-    }
-    fclose(arquivo);
-    return NULL;
-}
+
